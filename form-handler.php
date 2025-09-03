707f835a2d2622de119d3a5d4e0a94fbe524b82c
NadaAlaa97 <nalaa3662@gmail.com>
2025-09-03 23:09:13 +0300

    added php mailer.

--- form-handler.php
+++ form-handler.php
@@ -1,23 +1,44 @@
 <?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-$email_from = 'info@university-website-theta.vercel.app';
-$email_subject = 'New Form Submission';
-$email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                 "Subject: $subject.\n".
-                  "User Message: $message.\n";
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-$to = 'nalaa3662@gmail.com';
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
+    $mail = new PHPMailer(true);
 
+    try {
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'nadakhater97@gmail.com';  
+        $mail->Password = 'urvq qyya ajzt vcwu';  
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-mail($to,$email_subject,$email_body,$headers);
-header("Location: contact.html");
+        $mail->setFrom('nadakhater97@gmail.com', 'Website Contact Form');
+        $mail->addAddress('nalaa3662@gmail.com'); 
 
+        $mail->isHTML(true);
+        $mail->Subject = 'New Form Submission';
+        $mail->Body    = "
+            <b>User Name:</b> $name <br>
+            <b>User Email:</b> $visitor_email <br>
+            <b>Subject:</b> $subject <br>
+            <b>Message:</b> $message
+        ";
+
+        $mail->send();
+        header("Location: contact.html");
+    } catch (Exception $e) {
+        echo "Message could not be sent. Error: {$mail->ErrorInfo}";
+    }
+}
 ?>
